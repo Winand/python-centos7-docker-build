52f5729d8bc5615e31d48e525184e8f222ae9ffc
Andrey Makarov <winandfx@gmail.com>
2024-09-25 11:39:08 +0300

    Install OpenSSL into a separate folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ source scl_source enable rh-perl530
 VERSION="${PYTHON_VERSION?}"
 OPENSSL_VERSION="${OPENSSL_VERSION:-1.1.1w}"
 INSTALL_PATH="${INSTALL_PATH?}"
+SSL_INSTALL_PATH=$INSTALL_PATH/openssl-$OPENSSL_VERSION
 
 mkdir -p $INSTALL_PATH
 mkdir -p ~/build/output
@@ -20,7 +21,7 @@ OPENSSL_URL=https://openssl.org/source/old/$OPENSSL_VERSION/openssl-$OPENSSL_VER
 [ "3.0.0" = "`echo $OPENSSL_VERSION 3.0.0 | xargs -n1 | sort -V | head -n1`" ] && \
     OPENSSL_URL=https://github.com/openssl/openssl/releases/download/openssl-$OPENSSL_VERSION/openssl-$OPENSSL_VERSION.tar.gz
 curl -LO $OPENSSL_URL && tar xzf openssl-$OPENSSL_VERSION.tar.gz && pushd openssl-$OPENSSL_VERSION
-./config --prefix=$INSTALL_PATH && make && make install_sw  # install w/o docs https://stackoverflow.com/q/47136654
+./config --prefix=$SSL_INSTALL_PATH && make && make install_sw  # install w/o docs https://stackoverflow.com/q/47136654
 popd
 
 curl -O https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tar.xz && tar xf Python-$VERSION.tar.xz && pushd Python-$VERSION
@@ -29,20 +30,20 @@ curl -O https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tar.xz && tar
 ./configure --help | grep -q -e --disable-test-modules && NO_TESTMOD="--disable-test-modules"
 
 # https://github.com/python/cpython/issues/121992 OpenSSL 3 uses lib64 folder
-if [ -f "$INSTALL_PATH/lib/pkgconfig/openssl.pc" ]; then
+if [ -f "$SSL_INSTALL_PATH/lib/pkgconfig/openssl.pc" ]; then
     LIB_SSL=lib
-elif [ -f "$INSTALL_PATH/lib64/pkgconfig/openssl.pc" ]; then
+elif [ -f "$SSL_INSTALL_PATH/lib64/pkgconfig/openssl.pc" ]; then
     LIB_SSL=lib64
 else
     echo "Error: openssl.pc not found in either lib64 or lib" >&2
     exit 1
 fi
 # Python 3.10+ with-openssl-rpath https://github.com/python/cpython/issues/87632
-./configure --help | grep -q -e --with-openssl-rpath >/dev/null \
+./configure --help | grep -q -e --with-openssl-rpath \
     && SSL_RPATH="--with-openssl-rpath=auto" \
-    || SSL_RPATH="LDFLAGS=-Wl,-rpath=$INSTALL_PATH/$LIB_SSL"
+    || SSL_RPATH="LDFLAGS=-Wl,-rpath=$SSL_INSTALL_PATH/$LIB_SSL"
 ./configure --enable-optimizations $NO_STATICLIB $NO_TESTMOD --prefix=$INSTALL_PATH \
-            $SSL_RPATH PKG_CONFIG_PATH=$INSTALL_PATH/$LIB_SSL/pkgconfig
+            $SSL_RPATH PKG_CONFIG_PATH=$SSL_INSTALL_PATH/$LIB_SSL/pkgconfig
 make
 make altinstall
 popd
